9e415ae6a03ae485f97d075d87a436917a314cb1
ViniciusGbasilio <viniciusbasilio102@gmail.com>
2026-08-31 21:49:53 -0300

    ajustando o arquivo cliente

--- PP26_2/Calculadora/Cliente_202211250005.java
+++ PP26_2/Calculadora/Cliente_202211250005.java
@@ -1,19 +1,26 @@
-public class Cliente_202211250005 {
+import java.util.Scanner;
 
+public class Cliente_202211250005 {
     public static void main(String[] args) {
         Calculadora_202211250005 calc = new Calculadora_202211250005();
+        Scanner scanner = new Scanner(System.in);
 
-        double a = 10;
-        double b = 0;
+        System.out.print("Digite o valor de a: ");
+        double a = scanner.nextDouble();
 
-        double c = 50;
-        double d = 2;
+        System.out.print("Digite o valor de b: ");
+        double b = scanner.nextDouble();
 
         System.out.println("Soma: " + calc.somar(a, b));
-        System.out.println("Divisao: " + calc.dividir(c, d));
-        
-        /*System.out.println("Subtracao: " + calc.subtrair(a, b));
-        System.out.println("Multiplicacao: " + calc.multiplicar(a, b));*/
-        System.out.println("Divisao: " + calc.dividir(a, b));
+        System.out.println("Subtracao: " + calc.subtrair(a, b));
+        System.out.println("Multiplicacao: " + calc.multiplicar(a, b));
+
+        try {
+            System.out.println("Divisao: " + calc.dividir(a, b));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        scanner.close();
     }
 }
